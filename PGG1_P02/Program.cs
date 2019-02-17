473222fd02b8835a782818da3aa74aa5ce1a90de
Rafael Alfaro <alfa0015.rafael@gmail.com>
2019-02-17 12:47:09 -0600

    Parcial Terminado

---
+++ PGG1_P02/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace PGG1_P02
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            decimal b=0, at=0, ar=0, atriangulo=0, arectangulo=0;
+            string inputUser;
+            Console.WriteLine("Ingrese el Valor de la Base");
+            inputUser = Console.ReadLine();
+            if (String.IsNullOrEmpty(inputUser))
+            {
+                Console.WriteLine("No ingreso Ningun Valor");
+            }
+            else
+            {
+                if (Decimal.TryParse(inputUser, out b))
+                {
+                    b = Convert.ToDecimal(inputUser);
+                }
+                else
+                {
+                    Console.WriteLine("Ingrese solo numeros");
+                }
+
+            }
+            Console.WriteLine("Ingrese el Valor de la Altura del Triangulo");
+            inputUser = Console.ReadLine();
+            if (String.IsNullOrEmpty(inputUser))
+            {
+                Console.WriteLine("No ingreso Ningun Valor");
+            }
+            else
+            {
+                if (Decimal.TryParse(inputUser, out at))
+                {
+                    at = Convert.ToDecimal(inputUser);
+                }
+                else
+                {
+                    Console.WriteLine("Ingrese solo numeros");
+                }
+            }
+            Console.WriteLine("Ingrese el Valor de la Altura del Rectangulo");
+            inputUser = Console.ReadLine();
+            if (String.IsNullOrEmpty(inputUser))
+            {
+                Console.WriteLine("No ingreso Ningun Valor");
+            }
+            else
+            {
+                if (Decimal.TryParse(inputUser, out ar))
+                {
+                    ar = Convert.ToDecimal(inputUser);
+                }
+                else
+                {
+                    Console.WriteLine("Ingrese solo numeros");
+                }
+            }
+            atriangulo = Math.Abs((b * at) / 2);
+            arectangulo = Math.Abs(b * ar);
+            Console.WriteLine($"Area Del Terrono es: {Math.Abs(atriangulo + arectangulo)} Metros");
+        }
+    }
+}
